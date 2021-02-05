8eaa37ff879d13f0e0b3d1c1f6f3fddf5f05a752
Gregory Foster <brona90@gmail.com>
2021-02-05 21:38:07 +0000

    added correct parameters to nvm script

--- .zsh.after/nvm.zsh
+++ .zsh.after/nvm.zsh
@@ -2,7 +2,7 @@ setopt no_aliases
 
 export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  [ -s "$NVM_DIR/bash_completion" ] && \.
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 setopt aliases
 
