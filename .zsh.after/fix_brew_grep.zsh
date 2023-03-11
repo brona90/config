7cec93f39873278c201cd5f5fa6100a3401e8114
Gregory Foster <brona90@gmail.com>
2023-03-11 15:55:12 -0500

    fix brew call for linux

--- .zsh.after/fix_brew_grep.zsh
+++ .zsh.after/fix_brew_grep.zsh
@@ -1,3 +1,7 @@
-if [ -d "$(brew --prefix)/opt/grep/libexec/gnubin" ]; then
-      PATH="$(brew --prefix)/opt/grep/libexec/gnubin:$PATH"
+if hash brew 2>/dev/null; then
+  if [ -d "$(brew --prefix)/opt/grep/libexec/gnubin" ]; then
+    PATH="$(brew --prefix)/opt/grep/libexec/gnubin:$PATH"
+  fi
+else
+  exit 0
 fi
